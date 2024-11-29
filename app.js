abb173b3318737758a158cbdaf7fd601879ecb8b
Umer Mehmood <deanbusiness007@gmail.com>
2024-11-30 01:39:42 +0500

    stillinc

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ var cardDiv = document.getElementsByClassName("make-cards")[0];
 var pName = document.getElementById("nameInp");
 var pComp = document.getElementById("companyInp");
 var pCode = document.getElementById("codeInp");
+var err = document.getElementById("eR");
 
 function addProduct() {
+  if (pName.value === "" && pComp.value === "" && pCode.value === "") {
+    err.innerText = "please input these fields";
+    err.style.color = "red";
+    return;
+  }
+
+  err.innerText = "Add your product"
+  err.style.color = "black";
+
   var proDuct = document.createElement("div");
   proDuct.setAttribute("class", "p-div");
   cardDiv.appendChild(proDuct);
@@ -25,14 +35,14 @@ function addProduct() {
   var editBtn = document.createElement("button");
   editBtn.innerText = "Edit";
   proDuct.appendChild(editBtn);
-  
+
   var deleteBtn = document.createElement("button");
   deleteBtn.setAttribute("onclick", "deleteCard(event)");
   deleteBtn.innerText = "Delete";
   proDuct.appendChild(deleteBtn);
 }
 
-function deleteCard(del) {
-  del.target.parentElement.remove();
-  return
+function deleteCard(proDuct) {
+  proDuct.target.parentElement.remove();
+  return;
 }
