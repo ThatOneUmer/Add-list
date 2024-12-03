7f4eb647f42acdf36b45765e77d5091087857491
Muhammad Umer <deanbusiness007@gmail.com>
2024-12-03 05:23:08 +0500

    not comp

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function addtoCard(e) {
   e.preventDefault();
 
   var pName = document.getElementById("nameInp").value;
-  if (pName.value === "") {
+  if (pName === "") {
     err.innerText = "please input this field";
     err.style.color = "red";
     return;
@@ -39,51 +39,67 @@ function readData() {
   return JSON.parse(localStorage.getItem("Added User"));
 }
 
-
 function renderListItems() {
   for (let i = 0; i < addToData.length; i++) {
     cardDiv.innerHTML += `<div class = "p-div" style='display:flex;'>
-      <input type='text' value='${addToData[i].listText}' class='hide'/>
-      <p>User Name : ${addToData[i].listText}</p>
-      <button onClick='editItem(event,${addToData[i].id})'>Edit</button>
+      <input type='text' id="upD" placeholder="${addToData[i].listText}" class='hide'/>
+      <p id="show" >User Name : ${addToData[i].listText}</p>
+      <button id="btnHide3" onClick='editItem(event,${addToData[i].id})'>Edit</button>
       <button id="btnHide" onClick='deleteCard(event,${addToData[i].id})'>Delete</button>
-      <button id="btnHide2" style="display: none;" onClick='deleteCard(event,${addToData[i].id})'>cancel</button>
+      <button id="btnHide2" style="display: none;" onClick='cancelItem()'>cancel</button>
       </div>`;
   }
 }
 renderListItems();
 
 function editItem(e, id) {
-  document.getElementById('btnHide').style.display = 'none';
+  document.getElementById("btnHide").style.display = "none";
   document.getElementById("btnHide2").style.display = "block";
 
-  e.target.previousElementSibling.style.display = "none";
   e.target.previousElementSibling.style.display = "none";
   e.target.previousElementSibling.previousElementSibling.style.display =
     "block";
   e.target.innerText = "save";
   var editInpValue =
     e.target.previousElementSibling.previousElementSibling.value;
-  console.log(editInpValue);
   e.target.setAttribute("onClick", `updatedItem(${id}, ${editInpValue})`);
 }
 
-function updatedItem(id, inptValue) {
+function cancelItem() {
+  for (let i = 0; i < addToData.length; i++) {
+    if (document.getElementById("btnHide3").innerText === "save") {
+      document.getElementById("btnHide").style.display = "block";
+      document.getElementById("btnHide3").style.display = "block";
+      document.getElementById("btnHide3").innerText = "Edit";
+      document
+        .getElementById("btnHide3")
+        .setAttribute("onClick", `editItem(event, ${addToData[i].id})`);
+      document.getElementById("btnHide2").style.display = "none";
+      document.getElementsByClassName("hide")[0].style.display = "none";
+      document.getElementById("show").style.display = "block";
+      return
+    }
+  }
+}
+
+function updatedItem(id, inputVal) {
   for (let i = 0; i < addToData.length; i++) {
     if (addToData[i].id === id) {
-      addToData[i] = { ...addToData[i], listText: inptValue };
-      addedItems(addToData);
-      return;
+      document.getElementById("btnHide").style.display = "block";
+      document.getElementById("btnHide2").style.display = "none";
+      document.getElementById("btnHide3").style.display = "block";
+      document.getElementById("btnHide3").innerText = "Edit";
+      document.getElementById("show").style.display = "block";
+      addToData[i] = { ...addToData[i], listText: inputVal};
+      addedItems(addToData[i]);
+      return; 
     }
   }
 }
 
 function deleteCard(e, id) {
-  e.target.parentElement.remove();
   for (let i = 0; i < addToData.length; i++) {
-    if (addToData[i].id === id) {
-      localStorage.removeItem("Added User", addToData[i]);
-    }
+    document.getElementById("btnHide").parentElement.remove();
+    return
   }
-  return;
 }
